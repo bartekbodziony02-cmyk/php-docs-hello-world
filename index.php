6f145d83da2f61db3a0cc8dc92b0c6d8b0e98609
bartekbodziony02-cmyk <bartekbodziony02@gmail.com>
2025-09-03 21:19:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
-$request = $_SERVER['REQUEST_URI'];
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if ($request == "/" || $request == "/index.php") {
+if ($request === '/' || $request === '/index.php') {
+    // Strona główna
     echo "<h1>Witaj w aplikacji Azure App Service!</h1>";
     echo "<p>Mój numer indeksu to: " . getenv("STUDENT_ID") . "</p>";
 } else {
+    // Błąd 404
     http_response_code(404);
-    include("404.html");
+    if (file_exists("404.html")) {
+        include("404.html");
+    } else {
+        echo "<h1>404 - Ups! Strona nie istnieje</h1>";
+    }
 }
 ?>
