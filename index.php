52b53019f6e00c05bbc4bc4b001a714c5d46d4f1
bartekbodziony02-cmyk <bartekbodziony02@gmail.com>
2025-09-03 21:12:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-echo "<h1>Witaj w aplikacji Azure App Service!</h1>";
-echo "<p>Mój numer indeksu to: " . getenv("STUDENT_ID") . "</p>";
+$request = $_SERVER['REQUEST_URI'];
+
+if ($request == "/" || $request == "/index.php") {
+    echo "<h1>Witaj w aplikacji Azure App Service!</h1>";
+    echo "<p>Mój numer indeksu to: " . getenv("STUDENT_ID") . "</p>";
+} else {
+    http_response_code(404);
+    include("404.html");
+}
 ?>
